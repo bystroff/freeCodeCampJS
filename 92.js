bf0da718bbca3634b5a3c3c0f2cb5c9453d5f7db
Kirill Bystrov <pes13@yandex.ru>
2022-11-08 10:13:27 +0300

    task 92 done

---
+++ 92.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "ABBA",
+      "title": "Ring Ring",
+      "release_year": 1973,
+      "formats": [
+        "CS",
+        "8T",
+        "LP",
+      "CD",
+    ]
+    }
+  ];
